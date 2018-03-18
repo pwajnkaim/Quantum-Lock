a538a8445e4deda5731bb9a9856485c3a4c3f9b9
Peter Wajnkaim <pwajnkaim@yahoo.co.uk>
2018-03-18 15:28:57 +0000

    MileStone 2

--- src/quantumLock/QuantumLock.java
+++ src/quantumLock/QuantumLock.java
@@ -154,7 +154,7 @@ public class QuantumLock extends KeyAdapter implements ActionListener{
 
         //debug
         //view.setGridResolution(1);
-        DebugViewer debugView = new DebugViewer(world, 500, 500);
+        //DebugViewer debugView = new DebugViewer(world, 500, 500);
     }
 
     public static void main(String[] args) {
